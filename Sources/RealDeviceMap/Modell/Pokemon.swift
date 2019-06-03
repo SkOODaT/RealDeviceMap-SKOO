78c2ef4e3ee2898e7e4e9d0255fe7b0bf0912e9f
SkOODaT <SkOODaT@users.noreply.github.com>
2019-06-02 20:45:40 -0400

    Fix IV Queue if EncounterResponse is parsed first

--- Sources/RealDeviceMap/Modell/Pokemon.swift
+++ Sources/RealDeviceMap/Modell/Pokemon.swift
@@ -310,8 +310,6 @@ class Pokemon: JSONConvertibleObject, WebHookEvent, Equatable, CustomStringConve
             }
             firstSeenTimestamp = updated
             
-            WebHookController.global.addPokemonEvent(pokemon: self)
-            InstanceController.global.gotPokemon(pokemon: self)
             let sql = """
                 INSERT INTO pokemon (id, pokemon_id, lat, lon, spawn_id, expire_timestamp, atk_iv, def_iv, sta_iv, move_1, move_2, cp, level, weight, size, gender, form, weather, costume, pokestop_id, updated, first_seen_timestamp, changed, cell_id, expire_timestamp_verified)
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), ?, ?)
@@ -453,7 +451,15 @@ class Pokemon: JSONConvertibleObject, WebHookEvent, Equatable, CustomStringConve
             Log.error(message: "[POKEMON] Failed to execute query. (\(mysqlStmt.errorMessage())")
             throw DBController.DBError()
         }
-        
+
+        if oldPokemon == nil {
+            WebHookController.global.addPokemonEvent(pokemon: self)
+            InstanceController.global.gotPokemon(pokemon: self)
+            if self.atkIv != nil {
+                InstanceController.global.gotIV(pokemon: self)
+            }
+        }
+   
     }
     
     public static func getAll(mysql: MySQL?=nil, minLat: Double, maxLat: Double, minLon: Double, maxLon: Double, showIV: Bool, updated: UInt32, pokemonFilterExclude: [Int]?=nil, pokemonFilterIV: [String: String]?=nil) throws -> [Pokemon] {
