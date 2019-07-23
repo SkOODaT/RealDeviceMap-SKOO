a2b48d22e833609520cf7c968ab9054d3325d7a3
SkOODaT <8633932+SkOODaT@users.noreply.github.com>
2019-07-22 22:44:46 -0400

    Update Costume Proto

--- Sources/RealDeviceMap/Misc/POGOProtos_Enums_Costume.swift
+++ Sources/RealDeviceMap/Misc/POGOProtos_Enums_Costume.swift
@@ -11,7 +11,6 @@ import POGOProtos
 extension POGOProtos_Enums_Costume {
     
     public static var allCases: [POGOProtos_Enums_Costume] = [
-        .unset,
         .holiday2016,
         .anniversary,
         .oneYearAnniversary,
@@ -25,34 +24,31 @@ extension POGOProtos_Enums_Costume {
     ]
     
     static var allCostumesInString: [String] {
-        
         var costumeStrings = [String]()
         for costume in POGOProtos_Enums_Costume.allCases {
-            costumeStrings.append(costume.costumeString)
+            for costumeString in costume.costumeStrings {
+                costumeStrings.append(costumeString)
+            }
         }
         return costumeStrings
-        
     }
     
-    var costumeString: String {
+    var costumeStrings: [String] {
         
         switch self {
-        case .holiday2016: return "25-1"
-        case .anniversary: return "25-2"
-        case .oneYearAnniversary: return "25-3"
-        case .halloween2017: return "25-4"
-        case .summer2018: return "25-5"
-        case .fall2018: return "25-6"
-        case .november2018: return "25-7"
-        case .winter2018: return "25-8"
-        case .feb2019: return "25-9"
-        case .may2019Noevolve: return "25-10"
-        case .unset:
-            return ""
-        case .UNRECOGNIZED(_):
-            return ""
-        }
-        
+        case .holiday2016: return ["25-1"]
+        case .anniversary: return ["25-2"]
+        case .oneYearAnniversary: return ["25-3"]
+        case .halloween2017: return ["25-4"]
+        case .summer2018: return ["25-5"]
+        case .fall2018: return ["25-6"]
+        case .november2018: return ["25-7"]
+        case .winter2018: return ["25-8"]
+        case .feb2019: return ["25-9"]
+        case .may2019Noevolve: return ["25-10"]
+        case .unset: return []
+        case .UNRECOGNIZED: return []
+		}
     }
     
 }
