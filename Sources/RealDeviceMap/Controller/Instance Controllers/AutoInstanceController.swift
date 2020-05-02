fb3b864f9b5aa5a13261e7a5e984ec044a8d4562
SkOODaT <8633932+SkOODaT@users.noreply.github.com>
2020-05-02 15:48:54 -0400

    Attempt To Fix SwiftLint

--- Sources/RealDeviceMap/Controller/Instance Controllers/AutoInstanceController.swift
+++ Sources/RealDeviceMap/Controller/Instance Controllers/AutoInstanceController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Florian Kostenzer on 23.10.18.
 //
-//  swiftlint:disable:next superfluous_disable_command
 //  swiftlint:disable file_length type_body_length function_body_length cyclomatic_complexity
 
 import Foundation
