e59eb682f684726f64a1cd142a4861bd57a5725c
SkOODaT <8633932+SkOODaT@users.noreply.github.com>
2020-08-18 13:32:55 -0400

    Account for suspended in isValid

--- Sources/RealDeviceMap/Modell/Account.swift
+++ Sources/RealDeviceMap/Modell/Account.swift
@@ -254,11 +254,15 @@ class Account: WebHookEvent {
     }
 
     public func isValid(ignoringWarning: Bool=false, group: String?=nil) -> Bool {
+        let now = UInt32(Date().timeIntervalSince1970)
         return (
             self.group == group &&
             (self.failed == nil || (
                 self.failed! == "GPR_RED_WARNING_2" &&
-                (ignoringWarning || (self.warnExpireTimestamp ?? UInt32.max) <= UInt32(Date().timeIntervalSince1970))
+                (ignoringWarning || (self.warnExpireTimestamp ?? UInt32.max) <= now)
+            ) || (
+                self.failed! == "suspended" &&
+                ((self.failedTimestamp ?? UInt32.max) <= now - 2592000)
             ))
         )
     }
